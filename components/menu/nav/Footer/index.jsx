2fdd0d44fbe90ca317610f99bc998574526008da
Monyvann <m.men6603@gmail.com>
2025-10-29 19:04:26 -0500

    Fix: add hyperlink to social

--- components/menu/nav/Footer/index.jsx
+++ components/menu/nav/Footer/index.jsx
@@ -1,11 +1,30 @@
+import Link from "next/link";
 import styles from "./style.module.scss";
 
 export default function index() {
   return (
     <div className={styles.footer}>
-      <a>Github</a>
-      <a>Instagram</a>
-      <a>LinkedIn</a>
+      <Link
+        target="_blank"
+        href="https://github.com/MonyVannn"
+        className="hover:underline"
+      >
+        Github
+      </Link>
+      <Link
+        target="_blank"
+        href="https://www.instagram.com/_monyvann_/"
+        className="hover:underline"
+      >
+        Instagram
+      </Link>
+      <Link
+        target="_blank"
+        href="https://www.linkedin.com/in/monyvann-men-65b7a5260"
+        className="hover:underline"
+      >
+        LinkedIn
+      </Link>
     </div>
   );
 }
